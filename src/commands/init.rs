ed8ad7e4c3cb300e105f065ba82445b582af3d3c
Federico <16429579+rawnly@users.noreply.github.com>
2026-03-24 15:40:19 +0100

    fix: print

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -3,5 +3,5 @@ use crate::shell;
 pub fn exec(shell: Option<shell::Shell>) {
     let shell = shell.or(shell::detect());
     let hook = shell::generate_hook(shell.unwrap_or_default());
-    eprintln!("{hook}")
+    println!("{hook}")
 }
